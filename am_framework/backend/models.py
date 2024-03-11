c43345516ce3dd2d18b91a35f729c957ff3bcbf6
Nathan <nbaumeister@wisc.edu>
2024-03-11 15:00:46 -0500

    Started on Adding Tables to Django models.py

--- am_framework/backend/models.py
+++ am_framework/backend/models.py
@@ -5,22 +5,34 @@ from django.db import models
 class aircrafttable(models.Model):
     aircraftId = models.AutoField(primary_key=True)
     tailNumber = models.CharField(max_length=45, null=True)
-    shipNumber = models.IntegerField(null=True)
-    Type = models.CharField(max_length=4, null=True) #TODO RENAME TYPE
+    #Define a Django Enum type for the aircraft
+    class aType(models.TextChoices):
+        A320 = "A320"
+        A321 = "A321"
+        A330 = "A330"
+        A350 = "A350"
+        B737 = "B737"
+        B757 = "B757"
+        B767 = "B767"
+        B787 = "B787"
+        B777 = "B777"
+    aircraftType = models.CharField(choices= aType, null=True)
+    Type = models.CharField(max_length=4, null=True)
     status = models.CharField(max_length=45, null=True)
     location = models.CharField(max_length=4, null=True)
 
 class airporttable(models.Model):
     airportId = models.AutoField(primary_key =True)
     airportCode = models.CharField(max_length = 4)
-    lattitude = models.CharField(max_length = 5) #TODO FIX THIS
+    latitude = models.CharField(max_length = 5)
     longitude = models.CharField(max_length = 5)
     numAircraft = models.IntegerField(null = True)
     userId = models.IntegerField(null = True)
 
 class movementtable(models.Model):
     movementId = models.AutoField(primary_key = True)
-    airportId = models.IntegerField(null = True) #TODO ADD ARRIVAL AND DEPARTURE
+    arrivalAirportId = models.IntegerField(null = True)
+    originAirportId = models.IntegerField(null = True)
     arrivalDate = models.CharField(max_length = 8)
     departureDate = models.CharField(max_length = 8)
     aircraftId = models.IntegerField(null = True)
@@ -30,7 +42,12 @@ class userprofile(models.Model):
     userId = models.IntegerField(primary_key =True)
     username = models.CharField(max_length = 45)
     password = models.CharField(max_length = 45)
-    role = models.CharField(max_length = 45)
+    #Limit roles to preset conditions
+    class roleChoice(models.TextChoices):
+        admin = "admin"
+        corporate = "corporate"
+        facility = "facility"
+    role = models.CharField(choices=roleChoice)
 
 
 # @qxu229: the model is not migrated to the database
