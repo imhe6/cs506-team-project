b93f97efa1193515f8506db028c6444623503d05
KUSUM GAUTAM <kgautam3@wisc.edu>
2024-03-18 16:24:12 -0500

    Update 4 files
    - /AircraftTracking/.gitkeep
    - /AircraftTracking/models.py
    - /AircraftTracking/urls.py
    - /AircraftTracking/views.py

--- AircraftTracking/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import record_aircraft_movement
-
-urlpatterns = [
-    path('api/record_movement/', record_aircraft_movement, name='record_aircraft_movement'),
-]
-
