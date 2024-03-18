ff5f731df6e6c84e4e79289a32c1b748cc0b5972
Nathan <nbaumeister@wisc.edu>
2024-03-18 11:45:10 -0500

    Aggregated data Started program to populate empty database

--- dataImport.py
+++ dataImport.py
@@ -36,18 +36,24 @@ def readcsv():
         val =(entry[0],entry[1],entry[2],entry[3])
         cursor.execute(sql,val)
         db.commit()
+        #CREATE INITIAL MOVEMENT TABLE ENTRY
+
+        cursor.execute(sql, val)
+        db.commit()
 
 
 
     #Add Airports to airporttable
     sql = "INSERT INTO airporttable(airportCode,user) VALUES(%s,%i)"
-    val =("ORD",adminId)
+    val =("KORD",adminId)
+    cursor.execute(sql,val)
     db.commit()
-    #Create initial movement logs for aircraft
-
-
-
-
+    val = ("KLAX", adminId)
+    cursor.execute(sql, val)
+    db.commit()
+    val = ("KJFK", adminId)
+    cursor.execute(sql, val)
+    db.commit()
 
 
 if __name__ == '__main__':
