897eeafedc1779445acc7a8dfe1440db1c98a205
SANG JUN BYUN <sbyun9@wisc.edu>
2024-03-18 22:41:52 -0500

    Upload New File

---
+++ aircraft-manager-frontend/src/pages/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataTable from '../components/DataTable';
+
+function DashboardPage() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <DataTable /> {/* DataTable 컴포넌트를 사용 */}
+        </div>
+    );
+}
+
+export default DashboardPage;
