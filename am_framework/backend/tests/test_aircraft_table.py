84402cfeabb81c6d2b6415216df72b54fbd65e05
Alvin CHENG <ccheng93@wisc.edu>
2024-04-03 20:40:44 -0500

    Can run unit test now

--- am_framework/backend/tests/test_aircraft_table.py
+++ am_framework/backend/tests/test_aircraft_table.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
-from backend.models import aircrafttable
+from rest_framework.test import APIClient
+from rest_framework import status
+from ..models import aircrafttable
 
 class AircraftTableGetTest(TestCase):
-    
+    def setUp(self):
+        self.instance = aircrafttable.objects.create(
+            tailNumber='123',aircraftType='A320',status='Departured',location='LAX')
+        self.client = APIClient()
+        
+    def test_get(self):
+        response = self.client.get('/api/aircraft/')
+        self.assertEqual(response.status_code, 200)
+        print(response)
