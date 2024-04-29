d0d7430baf9bcb58a61c5f589b9656e6211b6bbf
Qihan Xu <qxu229@wisc.edu>
2024-04-29 02:54:44 -0500

    linting

--- am_framework/am_framework/settings.py
+++ am_framework/am_framework/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--3a9)7#=8ecemdt)#n8a61zk2aclp72#-ksrkgjh$+9v1y()l*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# debug is set to True by default; 
+# debug is set to True by default;
 # set environment variable DEBUG to False in production
-DEBUG = str(getenv('DJANGO_DEBUG', True)) == 'True'
+DEBUG = str(getenv("DJANGO_DEBUG", True)) == "True"
 DEBUG = False
-ALLOWED_HOSTS = [  
-    'localhost',
-    '127.0.0.1',
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
 ]
 
 
